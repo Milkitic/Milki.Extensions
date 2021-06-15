1f94fa0e64cc2a0660d2190c0f873d74689b3451
Milkitic <Milkitic@126.com>
2021-06-15 17:34:52 +0800

    property changed

--- src/Milki.Extensions.MixPlayer/Configuration.cs
+++ src/Milki.Extensions.MixPlayer/Configuration.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.CompilerServices;
 using Microsoft.Extensions.Logging;
+using Milki.Extensions.MixPlayer.Annotations;
 
 namespace Milki.Extensions.MixPlayer
 {
-    public class Configuration
+    public class Configuration : INotifyPropertyChanged
     {
         private Configuration()
         {
@@ -14,6 +17,13 @@ namespace Milki.Extensions.MixPlayer
         public static Configuration Instance { get; } = new Configuration();
 
         private ILoggerFactory? _factory;
+        private uint _generalOffset = 0;
+        private float _playbackRate = 1;
+        private bool _keepTune = false;
+        private string _defaultDir = Path.Combine(Environment.CurrentDirectory, "default");
+        private string _cacheDir = Path.Combine(Environment.CurrentDirectory, "caching");
+        private string _soundTouchDir = Path.Combine(Environment.CurrentDirectory, "libs", "SoundTouch");
+
         internal ILogger<T>? GetLogger<T>() => _factory?.CreateLogger<T>();
         internal ILogger? GetLogger(Type category)
         {
@@ -21,7 +31,7 @@ namespace Milki.Extensions.MixPlayer
             return _factory?.CreateLogger(fullName);
         }
 
-        public ILogger? GetCurrentClassLogger()
+        internal ILogger? GetCurrentClassLogger()
         {
             StackTrace stackTrace = new StackTrace();
             StackFrame[]? stackFrames = stackTrace.GetFrames();
@@ -35,20 +45,83 @@ namespace Milki.Extensions.MixPlayer
             return null;
         }
 
-        public uint GeneralOffset { get; set; } = 0;
-        public float PlaybackRate { get; set; } = 1;
-        public bool KeepTune { get; set; } = false;
+        public uint GeneralOffset
+        {
+            get => _generalOffset;
+            set
+            {
+                if (value == _generalOffset) return;
+                _generalOffset = value;
+                OnPropertyChanged();
+            }
+        }
 
-        public string DefaultDir { get; set; } =
-            Path.Combine(Environment.CurrentDirectory, "default");
-        public string CacheDir { get; set; } =
-            Path.Combine(Environment.CurrentDirectory, "caching");
-        public string SoundTouchDir { get; set; } =
-            Path.Combine(Environment.CurrentDirectory, "libs", "SoundTouch");
+        public float PlaybackRate
+        {
+            get => _playbackRate;
+            set
+            {
+                if (value.Equals(_playbackRate)) return;
+                _playbackRate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool KeepTune
+        {
+            get => _keepTune;
+            set
+            {
+                if (value == _keepTune) return;
+                _keepTune = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string DefaultDir
+        {
+            get => _defaultDir;
+            set
+            {
+                if (value == _defaultDir) return;
+                _defaultDir = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string CacheDir
+        {
+            get => _cacheDir;
+            set
+            {
+                if (value == _cacheDir) return;
+                _cacheDir = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string SoundTouchDir
+        {
+            get => _soundTouchDir;
+            set
+            {
+                if (value == _soundTouchDir) return;
+                _soundTouchDir = value;
+                OnPropertyChanged();
+            }
+        }
 
         public void SetLogger(ILoggerFactory loggerFactory)
         {
             _factory = loggerFactory;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
