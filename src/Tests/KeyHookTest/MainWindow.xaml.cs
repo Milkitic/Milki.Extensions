bb7f252d5a849f66a35ae51f9b9bfa202aac9fff
Milkitic <Milkitic@126.com>
2025-05-21 17:39:44 +0800

    refactor(KeyHookTest): ♻️ 更新键盘钩子初始化逻辑
    修改了键盘钩子的初始化方式，使用原始输入创建钩子，并注释掉了其他创建方式。同时，更新了按键事件的输出格式，以便更清晰地显示按键的按下和抬起状态。

--- src/Tests/KeyHookTest/MainWindow.xaml.cs
+++ src/Tests/KeyHookTest/MainWindow.xaml.cs
@@ -16,13 +16,14 @@ namespace KeyHookTest
         public MainWindow()
         {
             InitializeComponent();
-            _keyboardHook = KeyboardHookFactory.CreateGlobal();
+            //_keyboardHook = KeyboardHookFactory.CreateGlobal();
             //_keyboardHook = KeyboardHookFactory.CreateApplication();
+            _keyboardHook = KeyboardHookFactory.CreateRawInput();
         }
 
         private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
         {
-            _keyboardHook.KeyPressed += KeyboardHook_KeyPressed;
+            //_keyboardHook.KeyPressed += KeyboardHook_KeyPressed;
             _handle = _keyboardHook.RegisterHotkey(
                 HookModifierKeys.Control, HookKeys.S, (modifier, key, type) =>
                 {
@@ -38,12 +39,16 @@ namespace KeyHookTest
             _keyboardHook.RegisterKey(HookKeys.Z, (modifier, key, type) =>
             {
                 if (type == KeyAction.KeyDown)
-                    Console.WriteLine($"[{GetThreadName()}] Z");
+                    Console.WriteLine($"[{GetThreadName()}] Z (\u2193)");
+                else if (type == KeyAction.KeyUp)
+                    Console.WriteLine($"[{GetThreadName()}] Z (\u2191)");
             });
             _keyboardHook.RegisterKey(HookKeys.X, (modifier, key, type) =>
             {
                 if (type == KeyAction.KeyDown)
-                    Console.WriteLine($"[{GetThreadName()}] X");
+                    Console.WriteLine($"[{GetThreadName()}] X (\u2193)");
+                else if (type == KeyAction.KeyUp)
+                    Console.WriteLine($"[{GetThreadName()}] X (\u2191)");
             });
             //_handle = _keyboardHook.RegisterKeyUp(
             //    HookKeys.O, (modifier, key, type) =>
